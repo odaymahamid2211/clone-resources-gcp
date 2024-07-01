320c46e6a3f12e6c59032705d58966fa5f373da4
OdayMahamid <oday.shibli.99@gmail.com>
2024-07-01 14:05:57 +0300

    skipping cloud function resourcse:\

--- CreateCloudRun.py
+++ CreateCloudRun.py
@@ -3,6 +3,7 @@ from google.cloud import run_v2
 from GetDetails import GetDetails
 from google.api_core.exceptions import NotFound
 
+
 class CloudRunCreator:
     def __init__(self, target_project):
         self.target_project = target_project
@@ -10,14 +11,22 @@ class CloudRunCreator:
 
     def create_cloud_run_services(self, cloud_run_details):
         for service_detail in cloud_run_details:
-            if self.service_exists(service_detail['name'], service_detail['location']):
-                logging.info(f"Service {service_detail['name']} already exists in location {service_detail['location']}. Skipping creation.")
+            # Check if the service originated from a Cloud Function
+            if any("gcf-artifacts" in image for image in service_detail['container_images']):
+                logging.info(f"Service {service_detail['name']} originated from a Cloud Function. Skipping creation.")
                 continue
+
+            if self.service_exists(service_detail['name'], service_detail['location']):
+                logging.info(
+                    f"Service {service_detail['name']} already exists in location {service_detail['location']}. Skipping creation.")
+                continue
+
             self.create_cloud_run_service(service_detail)
 
     def service_exists(self, service_name, location):
         try:
-            service = self.run_client.get_service(name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
+            service = self.run_client.get_service(
+                name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
             return True
         except NotFound:
             return False
@@ -38,7 +47,6 @@ class CloudRunCreator:
         template = run_v2.RevisionTemplate(
             containers=containers
         )
-
 
         container_resources = service_detail['container_resources']
         cpu = container_resources['cpu']
@@ -67,9 +75,11 @@ class CloudRunCreator:
 
     def delete_cloud_run_service(self, service_name, location):
         try:
-            self.run_client.delete_service(name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
+            self.run_client.delete_service(
+                name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
         except Exception as e:
             logging.error(f"An error occurred while deleting the service '{service_name}': {e}")
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
