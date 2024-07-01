3d7efc91992f82b744db62617952f940c6a94d3e
OdayMahamid <oday.shibli.99@gmail.com>
2024-07-01 09:11:48 +0300

    added Creation of cloud run methods

---
+++ CreateCloudRun.py
@@ -0,0 +1,75 @@
+import logging
+from google.cloud import run_v2
+from GetDetails import GetDetails
+from google.api_core.exceptions import NotFound
+
+class CloudRunCreator:
+    def __init__(self, target_project):
+        self.target_project = target_project
+        self.run_client = run_v2.ServicesClient()
+
+    def create_cloud_run_services(self, cloud_run_details):
+        for service_detail in cloud_run_details:
+            if self.service_exists(service_detail['name'], service_detail['location']):
+                logging.info(f"Service {service_detail['name']} already exists in location {service_detail['location']}. Skipping creation.")
+                continue
+            self.create_cloud_run_service(service_detail)
+
+    def service_exists(self, service_name, location):
+        try:
+            service = self.run_client.get_service(name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
+            return True
+        except NotFound:
+            return False
+        except Exception as e:
+            logging.error(f"An error occurred while checking if service '{service_name}' exists: {e}")
+            return False
+
+    def create_cloud_run_service(self, service_detail):
+        location = service_detail['location']
+        project = self.target_project
+        service_name = service_detail['name']
+
+        containers = []
+        for image in service_detail['container_images']:
+            container = run_v2.Container(image=image)
+            containers.append(container)
+
+        template = run_v2.RevisionTemplate(
+            containers=containers
+        )
+
+        service = run_v2.Service(
+            template=template,
+            ingress=run_v2.IngressTraffic.INGRESS_TRAFFIC_ALL
+        )
+
+        try:
+            operation = self.run_client.create_service(
+                parent=f"projects/{project}/locations/{location}",
+                service=service,
+                service_id=service_name
+            )
+            operation.result()
+            logging.info(f"Service {service_name} created successfully.")
+        except Exception as e:
+            logging.error(f"An error occurred while creating the service '{service_name}': {e}")
+            self.delete_cloud_run_service(service_name, location)
+
+    def delete_cloud_run_service(self, service_name, location):
+        try:
+            self.run_client.delete_service(name=f"projects/{self.target_project}/locations/{location}/services/{service_name}")
+        except Exception as e:
+            logging.error(f"An error occurred while deleting the service '{service_name}': {e}")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    source_project_id = 'wideops-support-393412'
+    target_project_id = 'wideops-internal-web-services'
+
+    get_details = GetDetails(source_project=source_project_id)
+    cloud_run_details = get_details.get_cloud_run_details()
+
+    cloud_run_creator = CloudRunCreator(target_project=target_project_id)
+    cloud_run_creator.create_cloud_run_services(cloud_run_details)
--- GetDetails.py
+++ GetDetails.py
@@ -94,12 +94,72 @@ class GetDetails:
 
         return instance_details_list
 
+    def get_cloud_run_details(self):
+        cloud_run_details_list = []
+
+        try:
+            project_resource = f"projects/{self.source_project}"
+            request = asset_v1.ListAssetsRequest(
+                parent=project_resource,
+                asset_types=['run.googleapis.com/Service'],
+                content_type=asset_v1.ContentType.RESOURCE
+            )
+
+            response = self.asset_client.list_assets(request=request)
+
+            for asset in response:
+                cloud_run_details = {}
+                if asset.resource:
+                    data = asset.resource.data
+                    metadata = data.get('metadata', {})
+                    spec = data.get('spec', {})
+                    status = data.get('status', {})
+
+                    containers = spec.get('template', {}).get('spec', {}).get('containers', [])
+                    container_images = [container.get('image', 'N/A') for container in containers]
+
+                    cloud_run_details = {
+                        'name': metadata.get('name', 'N/A'),
+                        'location': metadata.get('labels', {}).get('cloud.googleapis.com/location', 'N/A'),
+                        'url': status.get('address', {}).get('url', 'N/A'),
+                        'ingress': metadata.get('annotations', {}).get('run.googleapis.com/ingress', 'N/A'),
+                        'ingress_status': metadata.get('annotations', {}).get('run.googleapis.com/ingress-status',
+                                                                              'N/A'),
+                        'operation_id': metadata.get('annotations', {}).get('run.googleapis.com/operation-id', 'N/A'),
+                        'api_version': data.get('apiVersion', 'N/A'),
+                        'kind': data.get('kind', 'N/A'),
+                        'generation': data.get('generation', 'N/A'),
+                        'latest_revision': status.get('traffic', [{}])[0].get('latestRevision', False),
+                        'percent_traffic': status.get('traffic', [{}])[0].get('percent', 'N/A'),
+                        'latest_created_revision_name': status.get('latestCreatedRevisionName', 'N/A'),
+                        'latest_ready_revision_name': status.get('latestReadyRevisionName', 'N/A'),
+                        'container_images': container_images
+                    }
+                else:
+                    cloud_run_details = {
+                        'name': asset.name,
+                        'error': 'No resource data available for this asset.',
+                        'update_time': asset.update_time
+                    }
+                cloud_run_details_list.append(cloud_run_details)
+
+        except Exception as e:
+            logging.error(f"An error occurred while fetching Cloud Run details: {e}")
+
+        return cloud_run_details_list
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     source_project = 'wideops-support-393412'
     get_details = GetDetails(source_project)
-    instance_details_list = get_details.get_instance_details()
 
+    instance_details_list = get_details.get_instance_details()
     for instance_details in instance_details_list:
         print(instance_details)
+
+
+    # Get Cloud Run details
+    cloud_run_details_list = get_details.get_cloud_run_details()
+    for cloud_run_details in cloud_run_details_list:
+        print(cloud_run_details)
